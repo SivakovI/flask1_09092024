63e1f6659e0ff241f361df4f53ed02d49bfd75ec
Igor Sivakov <sivakov.igor@gmail.com>
2024-09-11 00:00:36 +0300

    Added /about url

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+about_me = {
+    "name": "Igor",
+    "email": "XJQp7@example.com",
+}
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/about")
+def about():
+    return about_me
 
 
 if __name__ == "__main__":
