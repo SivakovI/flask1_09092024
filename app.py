6066d08f9fb1d3b124cb4c9f72ebd6a159518038
Igor Sivakov <sivakov.igor@gmail.com>
2024-09-11 16:50:13 +0300

    Выполнены практические задания, часть 1

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from http import HTTPStatus
+from random import choice
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +9,40 @@ about_me = {
     "name": "Igor",
     "email": "XJQp7@example.com",
 }
+
+quotes = [
+    {
+        "id": 3,
+        "author": "Rick Cook",
+        "text": (
+            "Программирование сегодня — это гонка разработчиков программ, "
+            "стремящихся писать программы с большей и лучшей "
+            "идиотоустойчивостью, и вселенной, которая пытается создать "
+            "больше отборных идиотов. Пока вселенная побеждает."
+        ),
+    },
+    {
+        "id": 5,
+        "author": "Waldi Ravens",
+        "text": (
+            "Программирование на С похоже на быстрые танцы на только что "
+            "отполированном полу людей с острыми бритвами в руках."
+        ),
+    },
+    {
+        "id": 6,
+        "author": "Mosher’s Law of Software Engineering",
+        "text": (
+            "Не волнуйтесь, если что-то не работает. Если бы всё "
+            "работало, вас бы уволили."
+        ),
+    },
+    {
+        "id": 8,
+        "author": "Yoggi Berra",
+        "text": ("В теории, теория и практика неразделимы. На практике это не" "так."),
+    },
+]
 
 
 @app.route("/")
@@ -16,6 +53,29 @@ def hello_world():
 @app.route("/about")
 def about():
     return about_me
+
+
+@app.route("/quotes")
+def get_quotes():
+    return quotes
+
+
+@app.route("/quotes/<int:id>")
+def get_quote(id):
+    for quote in quotes:
+        if quote["id"] == id:
+            return quote
+    return f"Quote with id {id} not found", HTTPStatus.NOT_FOUND
+
+
+@app.route("/quotes/count")
+def get_quote_count():
+    return {"count": len(quotes)}
+
+
+@app.route("/quotes/random")
+def get_random_quote():
+    return choice(quotes)
 
 
 if __name__ == "__main__":
